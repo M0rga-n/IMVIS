0c996da85be9885a2a8655ac361f198bbbb1f28e
M0rga-n <verburgnoah@gmail.com>
2023-04-26 10:42:12 +0200

    first commit

---
+++ code/Histogram.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+class Histogram {
+public:
+	Histogram(const String& winname, Mat& src);
+	void update();
+	int getTrackbar();
+private:
+	int _pixelvalue = 127; // beginwaarde voor trackbar waarde
+	int histSize = 256; // Totaal aantal grijswaarden
+	String winname;
+	Mat src, dst, histI, histO, histImage;
+};
+
