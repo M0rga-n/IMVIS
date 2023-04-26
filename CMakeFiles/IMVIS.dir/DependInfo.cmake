2756d1b0711662d67b57a288cc9c997341f73c53
M0rga-n <verburgnoah@gmail.com>
2023-04-26 10:44:09 +0200

    cmake

---
+++ CMakeFiles/IMVIS.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/morgan/opdrachten/IMVIS/code/Histogram.cpp" "CMakeFiles/IMVIS.dir/code/Histogram.cpp.o" "gcc" "CMakeFiles/IMVIS.dir/code/Histogram.cpp.o.d"
+  "/home/morgan/opdrachten/IMVIS/code/Mirror.cpp" "CMakeFiles/IMVIS.dir/code/Mirror.cpp.o" "gcc" "CMakeFiles/IMVIS.dir/code/Mirror.cpp.o.d"
+  "/home/morgan/opdrachten/IMVIS/code/h-nse-imvis.cpp" "CMakeFiles/IMVIS.dir/code/h-nse-imvis.cpp.o" "gcc" "CMakeFiles/IMVIS.dir/code/h-nse-imvis.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/IMVIS.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IMVIS.dir/code/Histogram.cpp.o"
+  "CMakeFiles/IMVIS.dir/code/Histogram.cpp.o.d"
+  "CMakeFiles/IMVIS.dir/code/Mirror.cpp.o"
+  "CMakeFiles/IMVIS.dir/code/Mirror.cpp.o.d"
+  "CMakeFiles/IMVIS.dir/code/h-nse-imvis.cpp.o"
+  "CMakeFiles/IMVIS.dir/code/h-nse-imvis.cpp.o.d"
+  "bin/IMVIS"
+  "bin/IMVIS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IMVIS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
